a69b8f43c6029033bcbad74ee5e8ae1b7988b5cb
Neo_Tsang <neo@tsang.nu>
2024-11-26 12:14:44 +0100

    debug dungeonGen

--- Crypts of Eternity/Assets/Scenes/DungeonGenerator.cs
+++ Crypts of Eternity/Assets/Scenes/DungeonGenerator.cs
@@ -3,12 +3,12 @@ using UnityEngine.Tilemaps;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    public int gridWidth = 400;
-    public int gridHeight = 400;
+    public int gridWidth = 100;
+    public int gridHeight = 100;
 
-    public int maxRooms = 100;
-    public int minRoomSize = 20;
-    public int maxRoomSize = 50;
+    public int maxRooms = 20;
+    public int minRoomSize = 6;
+    public int maxRoomSize = 12;
 
     public Tilemap tilemap; // The Tilemap to paint on.
     public TileBase[] floorTiles; // Array to hold floor tiles.
@@ -20,19 +20,30 @@ public class DungeonGenerator : MonoBehaviour
 
     void Start()
     {
+        Debug.Log("Starting Dungeon Generation...");
+        
         grid = new int[gridWidth, gridHeight];
         roomGenerator = new RoomGenerator(grid, gridWidth, gridHeight);
         hallwayGenerator = new HallwayGenerator(grid);
 
         GenerateDungeon();
+        DebugGrid(); // Display the grid in the console for debugging.
         RenderDungeon();
     }
 
     void GenerateDungeon()
     {
+        Debug.Log("Generating Rooms...");
         roomGenerator.GenerateRooms(maxRooms, minRoomSize, maxRoomSize);
+        Debug.Log($"Rooms Generated: {roomGenerator.Rooms.Count}");
+
+        Debug.Log("Connecting Rooms with Hallways...");
         hallwayGenerator.ConnectRooms(roomGenerator.Rooms);
+        Debug.Log("Hallways Connected");
+
+        Debug.Log("Adding Walls...");
         AddWalls();
+        Debug.Log("Walls Added");
     }
 
     void AddWalls()
@@ -61,6 +72,8 @@ public class DungeonGenerator : MonoBehaviour
 
     void RenderDungeon()
     {
+        Debug.Log("Rendering Dungeon...");
+        
         for (int x = 0; x < gridWidth; x++)
         {
             for (int y = 0; y < gridHeight; y++)
@@ -69,19 +82,46 @@ public class DungeonGenerator : MonoBehaviour
 
                 if (grid[x, y] == 1) // Floor
                 {
-                    tilemap.SetTile(tilePosition, GetRandomTile(floorTiles));
+                    var tile = GetRandomTile(floorTiles);
+                    tilemap.SetTile(tilePosition, tile);
+                    Debug.Log($"Placed Floor at {tilePosition}");
                 }
                 else if (grid[x, y] == 2) // Wall
                 {
-                    tilemap.SetTile(tilePosition, GetRandomTile(wallTiles));
+                    var tile = GetRandomTile(wallTiles);
+                    tilemap.SetTile(tilePosition, tile);
+                    Debug.Log($"Placed Wall at {tilePosition}");
                 }
             }
         }
+
+        Debug.Log("Dungeon Rendered");
     }
 
     TileBase GetRandomTile(TileBase[] tiles)
     {
-        if (tiles.Length == 0) return null;
-        return tiles[Random.Range(0, tiles.Length)];
+        if (tiles.Length == 0)
+        {
+            Debug.LogWarning("Tile array is empty!");
+            return null;
+        }
+        var selectedTile = tiles[Random.Range(0, tiles.Length)];
+        Debug.Log("Selected Tile: " + selectedTile.name);
+        return selectedTile;
+    }
+
+    void DebugGrid()
+    {
+        Debug.Log("Grid Layout:");
+        string gridString = "";
+        for (int y = 0; y < gridHeight; y++)
+        {
+            for (int x = 0; x < gridWidth; x++)
+            {
+                gridString += grid[x, y] + " ";
+            }
+            gridString += "\n";
+        }
+        Debug.Log(gridString);
     }
 }
